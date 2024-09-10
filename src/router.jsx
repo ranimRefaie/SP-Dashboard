421cd5b9b67106a7fc396b114b20456909c6cd56
ranimRefaie <ranemalre@gmail.com>
2024-09-10 20:45:17 +0300

    first commit

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/SP-Dashboard",
     element: <SignIn />,
   },
   {

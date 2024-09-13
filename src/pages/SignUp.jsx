a2e6c9dfe441fe587dd2d496e98e4c9c47e7af54
ranimRefaie <ranemalre@gmail.com>
2024-09-13 03:13:23 +0300

    first commit

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -12,12 +12,12 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="flex w-10/12 mx-auto justify-between mt-10 items-center">
+    <div className="flex w-10/12 mx-auto justify-between mt-10">
 
       <div className="w-3/4 flex flex-col gap-8 ">
         <img src={logo} alt="" className="w-36 mb-7" />
 
-        <form className="flex w-3/4 flex-col gap-6 mt-5">
+        <form className="flex w-3/4 flex-col gap-6">
           <h2 className="text-2xl font-bold" style={{color:'#424242'}}>Create Account</h2>
           <div className="flex gap-8">
             <div className="flex flex-col gap-3">
